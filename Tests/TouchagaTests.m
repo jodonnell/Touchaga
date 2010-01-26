0660638aa75c5eb424aa9d590ecbf3906000bed1
Jacob O'Donnell <jacobodonnell@gmail.com>
2010-01-25 21:55:12 -0500

    added more tests

--- Tests/TouchagaTests.m
+++ Tests/TouchagaTests.m
@@ -10,6 +10,9 @@
 
 #import "Player.h"
 #import "cocos2d.h"
+#import "MainGameLoop.h"
+#import "WarpEnergy.h"
+#import "Globals.h"
 
 @implementation TouchagaTests
 @synthesize player;
@@ -17,6 +20,11 @@
 - (void) setUp
 {
     [self setPlayer: [[Player alloc] init]];
+}
+
+- (void) tearDown
+{
+    [player release];
 }
 
 - (void) testPlayerGameOver
@@ -30,4 +38,39 @@
     STAssertEquals([player position], ccp(60, 60), @"Player setMove did not set player.");
 }
 
+-(void) testWarpIn
+{
+    [player moveTo:ccp(60,60)];
+    [player warpIn:ccp(100, 100)];
+    STAssertEquals([player position], ccp(100, 100), @"Player warpIn did not set player.");
+    
+    [player warpOut];
+    [player warpIn:ccp(0, 0)];
+    STAssertEquals([player position], ccp(100, 100), @"Player warpIn warped the player into the shoot button.");
+}
+
+-(void) testScore
+{
+    [player addScore:20];
+    STAssertEquals([player score], 20, @"Player score was not set.");
+}
+
+-(void) testLoseLife
+{
+    STAssertFalse([player isGameOver], @"Should not be player game over.");
+    int lives = [player lives];
+    while ([player lives]) {
+	[player loseLife];
+	STAssertEquals([player lives], lives - 1, @"Player did not lose life.");
+	lives--;
+    }
+    STAssertTrue([player isGameOver], @"Should be player game over.");
+}
+
 @end
+
+
+
+/*
+ * Overide all touch functions with categories, isShooting 
+*/
