8057754058c7a526653f63cfa3284e0ba8c33447
Jacob O'Donnell <jacobodonnell@gmail.com>
2010-02-14 01:56:43 -0500

    added basic enemy test class

---
+++ Classes/Level/BasicEnemy.h
@@ -0,0 +1,18 @@
+//
+//  BasicEnemy.h
+//  Touchaga
+//
+//  Created by Jacob O'Donnell on 2/12/10.
+//  Copyright 2010 __MyCompanyName__. All rights reserved.
+//
+
+#import "Enemy.h"
+
+/**
+
+ */
+@interface BasicEnemy : Enemy {
+}
+
+
+@end
---
+++ Classes/Level/BasicEnemy.m
@@ -0,0 +1,12 @@
+//
+//  BasicEnemy.m
+//  MyFirstGame
+//
+//  Created by Jacob O'Donnell on 2/12/10.
+//  Copyright 2010 __MyCompanyName__. All rights reserved.
+//
+
+#import "BasicEnemy.h"
+
+@implementation BasicEnemy
+@end
